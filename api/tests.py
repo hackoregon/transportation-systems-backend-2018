f87b1a2c43dce905419f752da02fa1c7abd1d00f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-05-21 15:38:32 -0700

    code for this backend

--- api/tests.py
+++ api/tests.py
@@ -1,12 +1,3 @@
-from django.test import TestCase, SimpleTestCase, TransactionTestCase
+from django.test import TestCase
 
-
-# Django, Writing and Running Unit Tests: https://docs.djangoproject.com/en/2.0/topics/testing/overview/
-# Django, Automated Unit Testing Tutorial: https://docs.djangoproject.com/en/2.0/intro/tutorial05/
-
-class ExampleTestCase(TestCase):
-    def setUp(self):
-        pass
-
-    def test_example(self):
-        self.assertTrue(True)
+# Create your tests here.
--- transportation_systems_2018/wsgi.py
+++ transportation_systems_2018/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dead_songs project.
+WSGI config for transportation_systems_2018 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dead_songs.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transportation_systems_2018.settings")
 
 application = get_wsgi_application()
