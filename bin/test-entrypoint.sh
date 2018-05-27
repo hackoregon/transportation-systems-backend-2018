0ce800215beb71e1d3320d1617ca71aeb854fb1f
Brian Grant <brian.grant@urbanairship.com>
2018-05-27 10:38:33 -0700

    fixes testing in prod

--- bin/test-entrypoint.sh
+++ bin/test-entrypoint.sh
@@ -4,13 +4,6 @@ export PATH=$PATH:~/.local/bin
 
 set -e
 
-export PGPASSWORD=$POSTGRES_PASSWORD
-until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d postgres -c '\q'
-do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 5
-done
-
 >&2 echo "Postgres is up"
 
 
--- bin/test.sh
+++ bin/test.sh
@@ -6,10 +6,10 @@ if [ $# == 0 ]; then usage; fi
 while getopts ":dp" opt; do
     case "$opt" in
         d)
-          docker-compose -f development-docker-compose.yml run --entrypoint /code/bin/test-entrypoint.sh  api_development
-           ;;
+          docker-compose -f development-docker-compose.yml run --entrypoint /code/bin/test-entrypoint.sh api_development -p 8000
+          ;;
         p)
-          docker-compose -f production-docker-compose.yml run --entrypoint /code/bin/test-entrypoint.sh  $DOCKER_SERVICE
+          docker-compose -f production-docker-compose.yml run --entrypoint /code/bin/test-entrypoint.sh $DOCKER_SERVICE  -p 8000
           ;;
         *)
           usage
