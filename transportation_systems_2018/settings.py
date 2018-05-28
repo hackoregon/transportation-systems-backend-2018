b82529c3f5860b0a23c2db73eab400e4059b7217
Brian Grant <brian.grant@urbanairship.com>
2018-05-28 11:34:24 -0700

    try without compression

--- transportation_systems_2018/settings.py
+++ transportation_systems_2018/settings.py
@@ -322,7 +322,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
