f763272bdd2f325c14a662263b0d3ca9a56521ed
Ara <araoshinsky@gmail.com>
2018-06-13 15:49:06 -0700

    Add crashes

--- civic_sandbox/models.py
+++ civic_sandbox/models.py
@@ -13,3 +13,14 @@ class SafetyHotlineTickets(models.Model):
         managed = False
         db_table = 'safety_hotline_tickets'
         in_db = 'safety_hotline_tickets'
+
+class Crash(models.Model):
+    crash_id = models.IntegerField(primary_key=True)
+    crash_dt = models.DateTimeField()
+    geom_point = models.PointField(null=True)
+    
+
+    class Meta:
+        managed = False
+        db_table = 'crash'
+        in_db = 'odot_crash_data'
