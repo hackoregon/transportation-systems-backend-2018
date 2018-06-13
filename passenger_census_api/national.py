016239dcc0172403bc8c9418592668538ef7e0fb
Brian Grant <brian.grant@urbanairship.com>
2018-06-13 16:20:22 -0700

    add stops back and add national totals endpoint

---
+++ passenger_census_api/national.py
@@ -0,0 +1,63 @@
+import json
+
+national = json.dumps(
+[
+  {
+    "year": "2002",
+    "total_ridership_millions": "265"
+  },
+  {
+    "year": "2003",
+    "total_ridership_millions": "260"
+  },
+  {
+    "year": "2004",
+    "total_ridership_millions": "261"
+  },
+  {
+    "year": "2005",
+    "total_ridership_millions": "282"
+  },
+  {
+    "year": "2006",
+    "total_ridership_millions": "282"
+  },
+  {
+    "year": "2007",
+    "total_ridership_millions": "308"
+  },
+  {
+    "year": "2008",
+    "total_ridership_millions": "305"
+  },
+  {
+    "year": "2009",
+    "total_ridership_millions": "300"
+  },
+  {
+    "year": "2010",
+    "total_ridership_millions": "295"
+  },
+  {
+    "year": "2011",
+    "total_ridership_millions": "300"
+  },
+  {
+    "year": "2012",
+    "total_ridership_millions": "304"
+  },
+  {
+    "year": "2013",
+    "total_ridership_millions": "305"
+  },
+  {
+    "year": "2014",
+    "total_ridership_millions": "309"
+  },
+  {
+    "year": "2015",
+    "total_ridership_millions": "304"
+  },
+
+]
+ )
