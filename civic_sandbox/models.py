d4b5d0d56e594ad97eeb6dfbeee381251801d5c1
Ara <araoshinsky@gmail.com>
2018-06-13 17:55:52 -0700

    Remove description from endpoint (identifying information

--- civic_sandbox/models.py
+++ civic_sandbox/models.py
@@ -6,7 +6,7 @@ options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('in_db',)
 class SafetyHotlineTickets(models.Model):
     id = models.IntegerField(primary_key=True)
     date_created = models.DateField()
-    description = models.TextField(blank=True, null=True)
+    # description = models.TextField(blank=True, null=True) #TODO: will add description to endpoint once identifying info is removed from field. 
     geom_4326 = models.PointField()
 
     class Meta:
@@ -18,7 +18,7 @@ class Crash(models.Model):
     crash_id = models.IntegerField(primary_key=True)
     crash_dt = models.DateTimeField()
     geom_point = models.PointField(null=True)
-    
+
 
     class Meta:
         managed = False
