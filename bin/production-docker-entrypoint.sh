c00a218c82c5f5d0d160715e25a8fc98e100b84d
Brian Grant <brian.grant@urbanairship.com>
2018-06-18 00:33:15 -0700

    update stop events"

--- bin/production-docker-entrypoint.sh
+++ bin/production-docker-entrypoint.sh
@@ -8,7 +8,7 @@ echo Debug: $DEBUG
 
 # Pull in environment variables values from AWS Parameter Store, and preserve the exports
 # source usage per https://stackoverflow.com/q/14742358/452120
-source /code/bin/get-ssm-parameters.sh
+# source /code/bin/get-ssm-parameters.sh
 
 python -Wall manage.py collectstatic --noinput
 
