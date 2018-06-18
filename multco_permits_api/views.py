3e339501f42c1f64f81533ec5120610c5218e3c2
Brian Grant <brian.grant@urbanairship.com>
2018-06-18 10:57:15 -0700

    adds route frequency and searching of permits

--- multco_permits_api/views.py
+++ multco_permits_api/views.py
@@ -16,6 +16,7 @@ from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from multco_permits_api.models import CurrentPermits, ArchivedPermits
 from multco_permits_api.serializers import CurrentPermitsSerializer, ArchivedPermitsSerializer
 
+
 class CurrentPermitsViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset will provide a list of Current Multnomah County Work Permits
@@ -24,7 +25,9 @@ class CurrentPermitsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CurrentPermits.objects.all()
     serializer_class = CurrentPermitsSerializer
     filter_backends = (SearchFilter, OrderingFilter)
-    filter_fields = '__all__'
+    search_fields = ('city', 'city_state', 'comments', 'cross_street', 'direction', 'district', 'effect_date', 'entry_date', 'expiration_date', 'final_date', 'issue_date', 'location', 'permit_category', 'permit_id', 'state', 'street', 'street_number', 'street_type', 'type', 'zip_code')
+    ordering_fields = ('city', 'city_state', 'comments', 'cross_street', 'direction', 'district', 'effect_date', 'entry_date', 'expiration_date', 'final_date', 'issue_date', 'location', 'permit_category', 'permit_id', 'state', 'street', 'street_number', 'street_type', 'type', 'zip_code')
+
 
 class ArchivedPermitsViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -34,3 +37,5 @@ class ArchivedPermitsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ArchivedPermits.objects.all()
     serializer_class = ArchivedPermitsSerializer
     filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('applic_title', 'bond_expiration_date', 'bond_number', 'comments', 'commodity', 'daily_posting_date', 'duplicate', 'effect_date', 'entry_date', 'expiration_date', 'final_date', 'id', 'issue_date', 'jurisdiction', 'location', 'permit_count', 'permit_effect_date', 'permit_number', 'pkey')
+    ordering_fields = ('applic_title', 'bond_expiration_date', 'bond_number', 'comments', 'commodity', 'daily_posting_date', 'duplicate', 'effect_date', 'entry_date', 'expiration_date', 'final_date', 'id', 'issue_date', 'jurisdiction', 'location', 'permit_count', 'permit_effect_date', 'permit_number', 'pkey')
