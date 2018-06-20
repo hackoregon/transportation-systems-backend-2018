b8de57599ddd174c1ffca2874418560f223ee547
Brian Grant <brian.grant@urbanairship.com>
2018-06-19 20:42:55 -0700

    adds service availability

---
+++ passenger_census_api/service_availability.py
@@ -0,0 +1,42 @@
+import json
+
+availability = json.dumps(
+[
+  {
+    "date": 2009,
+    "trip_count": 3011563
+  },
+  {
+    "date": 2010,
+    "trip_count": 7986892
+  },
+  {
+    "date": 2011,
+    "trip_count": 9296612
+  },
+  {
+    "date": 2012,
+    "trip_count": 8740030
+  },
+  {
+    "date": 2013,
+    "trip_count": 8765957
+  },
+  {
+    "date": 2014,
+    "trip_count": 14307650
+  },
+  {
+    "date": 2015,
+    "trip_count": 11393043
+  },
+  {
+    "date": 2016,
+    "trip_count": 14269409
+  },
+  {
+    "date": 2017,
+    "trip_count": 12858326
+  }
+]
+)
