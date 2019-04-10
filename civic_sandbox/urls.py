2a6700aa1b864602ed3e458673032f1804d062bd
Nathan Miller <nmiller217@gmail.com>
2019-04-09 18:20:41 -0700

    add package definitions endpoint and data

--- civic_sandbox/urls.py
+++ civic_sandbox/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+from civic_sandbox import packages_view
 
 urlpatterns = [
     url(r'^slides/safetyhotline/(?P<date_filter>\d+)', views.safetyhotline),
@@ -10,5 +11,6 @@ urlpatterns = [
     url(r'^slides/routechange/', views.routechange),
     url(r'^foundations/blockchange/', views.blockchange),
     url(r'^slides/sensors/', views.sensors),
+    url(r'^package_info/', packages_view.packages_view, name='package_info'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
